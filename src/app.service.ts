68266fb7ebe1d76a672c2f3e030c2a55549e8caa
higorseixas <higor.c.seixas@gmail.com>
2023-03-09 10:45:49 -0300

    Adicionando configurações

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,29 +1,30 @@
 import { Injectable } from '@nestjs/common';
-import { Whatsapp, create, } from 'venom-bot';
+import { Whatsapp, create } from 'venom-bot';
 
 @Injectable()
 export class AppService {
-  private client: Whatsapp
+  private client: Whatsapp;
 
-  constructor() { this.initialize() }
+  constructor() {
+    this.initialize();
+  }
 
   async sendText(to: string, body: string) {
-    this.client.sendText(to, body)
-    .catch((error) => console.error(error))
+    this.client.sendText(to, body).catch((error) => console.error(error));
   }
 
-  qr = (base64Qrimg: string) => { }
-  status = (statusSession: string, session: string) => { }
+  qr = (base64Qrimg: string) => {};
+  status = (statusSession: string, session: string) => {};
   start = (client: Whatsapp) => {
-    const numberToSend = process.env.NUMBER_TO_SEND+'@c.us'
-    this.client = client
-    this.sendText(numberToSend, 'Olá, tudo bem?')
-  }
+    const numberToSend = process.env.NUMBER_TO_SEND + '@c.us';
+    this.client = client;
+    this.sendText(numberToSend, 'Olá, tudo bem?');
+  };
 
   private initialize() {
-    create("whatsapp-chat-bot", this.qr, this.status)
+    create('whatsapp-chat-bot', this.qr, this.status)
       .then((client) => this.start(client))
-      .catch((error) => console.error(error))
+      .catch((error) => console.error(error));
   }
 
   getHello(): string {
